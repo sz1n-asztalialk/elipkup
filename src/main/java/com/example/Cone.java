50b478b3299568987cfdc2450a2c6afe1bb1f54c
Dóra <doraboglarka@ktch.hu>
2026-04-21 12:21:09 +0100

    javafx funkcionál

---
+++ src/main/java/com/example/Cone.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Cone {
+    public static double calcVolume(double r1, double r2, double height){
+        return ((1.0/3.0)*Math.PI*r1*r2*height);
+    }
+}
--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -20,7 +20,18 @@ public class MainController {
 
     @FXML
     void onClickCountButton(ActionEvent event) {
+        this.startCalc();
+    }
 
+    void startCalc(){
+        
+        double radius1 = Double.parseDouble(radius1field.getText());
+        double radius2 = Double.parseDouble(radius2field.getText());
+        double height = Double.parseDouble(heightfield.getText());
+
+        double volume = Cone.calcVolume(radius1, radius2, height);
+
+        volumefield.setText(String.valueOf(volume));
     }
 
 }
