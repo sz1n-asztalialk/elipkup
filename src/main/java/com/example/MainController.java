531d06880708542b1969876122a64eac1318ad3b
Dóra <doraboglarka@ktch.hu>
2026-04-21 12:06:36 +0100

    fxml alap

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField heightfield;
+
+    @FXML
+    private TextField radius1field;
+
+    @FXML
+    private TextField radius2field;
+
+    @FXML
+    private TextField volumefield;
+
+    @FXML
+    void onClickCountButton(ActionEvent event) {
+
+    }
+
+}
